e9592e67db5d4211ac7ae987819bb40d332eed64
maggie-j-liu <maggie.j.liu@gmail.com>
2021-12-10 18:17:35 -0800

    firebase

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
+import { UserContextProvider } from '../utils/useUser'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <UserContextProvider><Component {...pageProps} /></UserContextProvider>
 }
 
 export default MyApp
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,35 @@
 import Head from "next/head";
+import useUser from "../utils/useUser";
 
 export default function Home() {
+  const { user, logout, signInWithGoogle } = useUser();
   return (
     <div>
       <Head>
-        <title>Next.js + TailwindCSS Starter</title>
+        <title>Next.js + TailwindCSS + Firebase Starter</title>
         <meta
           name="description"
-          content="A Next.js + TailwindCSS starter"
+          content="A Next.js + TailwindCSS + Firebase starter"
         />
       </Head>
-      <main className={"flex items-center justify-center w-screen h-screen"}>
+      <main
+        className={
+          "flex flex-col items-center justify-center w-screen h-screen"
+        }
+      >
         <h1 className={"text-4xl font-bold text-indigo-800 hover:italic"}>
-          A Next.js + TailwindCSS Starter
+          A Next.js + TailwindCSS + Firebase Starter
         </h1>
+        {user ? (
+          <>
+            <p>Signed in as {user.displayName}</p>
+            <button onClick={() => logout()}>Sign Out</button>
+          </>
+        ) : (
+          <button onClick={() => signInWithGoogle()}>
+            Sign in with Google
+          </button>
+        )}
       </main>
     </div>
   );
